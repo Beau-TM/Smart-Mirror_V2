18b4a246f5a50af5a990ff3b968cef974908a3e5
lenndehert <151083967+lenndehert@users.noreply.github.com>
2026-04-29 09:40:48 +0200

    iets geplakt

--- code.cpp
+++ code.cpp
@@ -1,10 +1,12 @@
+//-------Libraries--------
+#include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
 #include <WiFi.h>
 #include "time.h"
 
-// --- CONFIGURATIE ---
+//-------Configuratie-------
 const char* ssid     = "OnePlus Nord 3 5G";
 const char* password = "y893cmpn";
 
@@ -22,10 +24,11 @@ const int   daylightOffset_sec = 3600;
 bool bewegingGezien = false;
 
 void setup() {
+  //-----Seriele motor startten------
   Serial.begin(115200);
   pinMode(PIR_PIN, INPUT);
   dht.begin();
-  
+  //--------Lcd opstartten---------
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
@@ -34,6 +37,7 @@ void setup() {
   // WiFi met een timeout (zodat de code niet vastloopt als wifi faalt)
   WiFi.begin(ssid, password);
   int counter = 0;
+  //-------tijd instellen als wifi werkt-------
   while (WiFi.status() != WL_CONNECTED && counter < 20) {
     delay(500);
     Serial.print(".");
@@ -46,7 +50,7 @@ void setup() {
   } else {
     Serial.println("\nWiFi mislukt, we gaan door zonder tijd.");
   }
-
+  //-------LCD uitzetten tot de er bewiging is--------
   lcd.clear();
   lcd.noBacklight();
 }
@@ -68,13 +72,13 @@ void toonGroet() {
 
 void lusInformatie() {
   while(digitalRead(PIR_PIN) == HIGH) {
-    // --- STAP 1: TIJD TONEN ---
+    //-----eerst tijd tonen-------
     struct tm timeinfo;
     if(getLocalTime(&timeinfo)) {
       lcd.clear();
-      lcd.setCursor(0, 0);
+      lcd.setCursor(3, 0);
       lcd.print("Het is nu:");
-      lcd.setCursor(0, 1);
+      lcd.setCursor(4, 1);
       char tijdStr[9];
       sprintf(tijdStr, "%02d:%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
       lcd.print(tijdStr);
@@ -82,20 +86,30 @@ void lusInformatie() {
     }
     if(digitalRead(PIR_PIN) == LOW) break;
 
-    // --- STAP 2: TEMP & VOCHT TONEN ---
+    //-------temperatuur en vocht tonen------
     float h = dht.readHumidity();
     float t = dht.readTemperature();
 
     // Check of de sensor wel werkt (geen 'nan')
     if (!isnan(h) && !isnan(t)) {
       lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Temp: "); lcd.print(t, 1); lcd.print("C");
-      lcd.setCursor(0, 1);
-      lcd.print("Vocht: "); lcd.print(h, 0); lcd.print("%");
+      // --- Temperatuur ---
+      char tempStr[16];
+      sprintf(tempStr, "Temp: %.1fC", t);
+      int startTemp = (16 - strlen(tempStr)) / 2;
+      lcd.setCursor(startTemp, 0);
+      lcd.print(tempStr);
+
+      // --- Vocht ---
+      char vochtStr[16];
+      sprintf(vochtStr, "Vocht: %.0f%%", h);
+      int startVocht = (16 - strlen(vochtStr)) / 2;
+      lcd.setCursor(startVocht, 1);
+      lcd.print(vochtStr);
+
       delay(3000);
 
-      // --- STAP 3: ALLEEN ADVIES ALS VOCHT > 65% ---
+      //-------advies als vocht >65% ---
       if (h >= 65.0) {
         lcd.clear();
         lcd.setCursor(0, 0);
